191161cbfd839ddf5300d0c14dd2ba46eeb2b643
Enokd <edragon851@students.mchenry.edu>
2024-01-20 18:53:54 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var myByte: Byte = 7
+    var myShort: Short = 22
+    var myInt: Int = 6
+    var myLong: Long = 7171717
+    var myFloat: Float = 7.7731F
+    var myDouble: Double = 171.0928
+    var myBool: Boolean = true
+    var myChar = 'g'
+
+    print("Byte demo: $myByte")
+    print("\nShort demo: $myShort")
+    print("\nInteger demo: $myInt")
+    print("\nLong demo: $myLong")
+    print("\nFloat demo: $myFloat")
+    print("\nDouble demo: $myDouble")
+    print("\nBoolean demo: $myBool")
+    print("\nCharacter demo:$myChar")
+}
